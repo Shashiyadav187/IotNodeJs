d6efaf5d0b02d77b6a601b6df359bad9d2f5d34f
ha jong soon <ha10000@gmail.com>
2017-10-31 09:04:38 +0900

    first comment

---
+++ hello.js
@@ -0,0 +1,26 @@
+var http = require('http');
+var fs = require('fs');
+var	 length = 0;
+fs.readFile('./hello.js', function(err,data){
+	if( err){
+		console.log(err);
+	}else{
+		console.log(data);
+		// console.log(data.toString());
+		length = data.toString().length;
+console.log("in--------------->length="+length);
+	}
+});
+
+console.log("out------------>length="+length);
+
+http.createServer(function handler(req, res){
+	console.log(req.method+','+ req.url);
+	// console.log('callback');
+	// console.log(req);
+	// console.log('req!' + req);
+	res.writeHead(200, {'Context-Type' : 'text/plain'});
+	res.end('Hello World\n');
+}).listen(80);
+
+console.log('Server running at port 80');
